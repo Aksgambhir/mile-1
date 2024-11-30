8114c4592bd497d26f1210b237bf6f2d16296e6a
Aksgambhir <akshay.gambhira@gmail.com>
2024-11-30 23:10:34 +0530

    intial commit

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,15 +6,7 @@ function Header() {
   return (
     <header className="flex flex-col">
       <div className="px-2 flex items-center justify-between text-stone-200 bg-stone-800">
-        <p className="text-sm font-light">
-          Made By:<span className="font-medium">Yousef Tarek</span>
-        </p>
-        <a
-          className="underline text-sm"
-          href="https://github.com/youseftrek/react-food-delivery"
-        >
-          Github repo &rarr;
-        </a>
+       
       </div>
       <div className="flex items-center justify-between bg-yellow-400 uppercase px-4 sm:px-6 py-3 border-b border-stone-300">
         <Link
